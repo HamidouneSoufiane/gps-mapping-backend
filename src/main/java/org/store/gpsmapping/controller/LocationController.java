7503d78a2015517d1242eb95cb39313ea2d85670
Soufiane Hamidoune <shamidoune97@gmail.com>
2024-10-21 23:04:53 +0100

    final 2

--- src/main/java/org/store/gpsmapping/controller/LocationController.java
+++ src/main/java/org/store/gpsmapping/controller/LocationController.java
@@ -1,10 +1,9 @@
 package org.store.gpsmapping.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.store.gpsmapping.entity.Location;
 import org.store.gpsmapping.service.LocationService;
 
@@ -18,5 +17,11 @@ public class LocationController {
     @GetMapping
     public ResponseEntity<List<Location>> getAllLocations() {
         return ResponseEntity.ok(locationService.getAllLocations());
+    }
+
+    @PostMapping
+    public ResponseEntity<Location> addLocation(@RequestBody Location location) {
+        Location savedLocation = locationService.saveLocation(location);
+        return new ResponseEntity<>(savedLocation, HttpStatus.CREATED);
     }
 }
